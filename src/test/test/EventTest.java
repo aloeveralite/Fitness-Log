51dddbd2c9bad8d6f60485a81b3e4572b28f33df
chrispark <cpark10@students.cs.ubc.ca>
2023-12-01 09:06:02 -0800

    Phase 4: Covered all branches for tests

--- src/test/test/EventTest.java
+++ src/test/test/EventTest.java
@@ -8,28 +8,59 @@ import org.junit.jupiter.api.Test;
 import java.util.Calendar;
 import java.util.Date;
 
-import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.*;
 
 // Represents tests for the event class
 public class EventTest {
     private Date testDate;
     private Event testEvent;
+    private Event testEvent2;
 
     @BeforeEach
     void runBefore() {
-        testEvent = new Event("Add exercise to log");
+        testEvent = new Event("Added exercise");
+        testEvent2 = new Event("Selected an exercise");
         testDate = Calendar.getInstance().getTime();
     }
 
     @Test
     void testConstructor() {
-        assertEquals("Add exercise to log", testEvent.getDescription());
+        assertEquals("Added exercise", testEvent.getDescription());
         assertEquals(testDate, testEvent.getDate());
     }
 
     @Test
+    void testEqualsNull() {
+        assertFalse(testEvent.equals(null));
+    }
+
+    @Test
+    void testEqualsDifferentClass() {
+        assertFalse(testEvent.equals(testDate));
+    }
+
+    @Test
+    void testEqualsMismatch() {
+        assertFalse(testEvent.equals(testEvent2));
+    }
+
+    @Test
+    void testEquals() {
+        assertTrue(testEvent.equals(testEvent));
+        assertTrue(testEvent2.equals(testEvent2));
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals((13 * testEvent.getDate().hashCode()
+                + testEvent.getDescription().hashCode()), testEvent.hashCode());
+        assertEquals((13 * testEvent2.getDate().hashCode()
+                + testEvent2.getDescription().hashCode()), testEvent2.hashCode());
+    }
+
+    @Test
     void testToString() {
-        assertEquals(testDate.toString() + "\n" + "Add exercise to log", testEvent.toString());
+        assertEquals(testDate.toString() + "\n" + "Added exercise", testEvent.toString());
     }
 }
 
